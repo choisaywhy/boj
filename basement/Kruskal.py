8c7de57e20bf52cce74a74f0449b0ea1e558fcb3
choisaywhy <sehwa98@icloud.com>
2022-10-15 15:35:43 +0900

    221015

--- basement/Kruskal.py
+++ basement/Kruskal.py
@@ -1,51 +1,88 @@
-# 서로소 집합 알고리즘
+# 크루스칼 알고리즘 구현 코드
 # reference // https://techblog-history-younghunjo1.tistory.com/262
 
-# import sys
-# input = sys.stdin.readline
-
-
 import sys
+import heapq
+input = sys.stdin.readline
 
-v, e = map(int, input().split())
-# 부모 테이블 초기화
-parent = [0] * (v+1)
-for i in range(1, v+1):
-    parent[i] = i
+V, E = map(int, input().split())
+parent = [i for i in range(V+1)]
+edge = heapq.heapify([])
+for _ in range(E) :
+    heapq.heappush(edge, tuple(map(int, input().split())))# cost, x, y 받기
+# edge = [tuple(map(int, input().split())) for _ in range(E)] 
 
-# find 연산
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
+def find(parent, x) :
+    if parent[x] != x :
+        parent[x] = find(parent, parent[x])
     return parent[x]
 
-# union 연산
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+def union(parent, x, y): # 이미 x, y가 다른 부모를 가지고 있다는 걸 상정하고 실행
+    x = find(parent, x)
+    y = find(parent, y)
 
-# 간선 정보 담을 리스트와 최소 신장 트리 계산 변수 정의
-edges = []
-total_cost = 0
+    parent[max(x,y)] = min(x,y)
+    return parent
 
-# 간선 정보 주어지고 비용을 기준으로 정렬
-for _ in range(e):
-    a, b, cost = map(int, input().split())
-    edges.append((cost, a, b))
+cost = 0
+for _ in range(E) :
+    c, x, y = heapq.heappop(edge)
+    if find(parent,x) == find(parent, y) :
+        continue
+    parent = union(parent, x,y)
+    cost += c
 
-# 간선 정보 비용 기준으로 오름차순 정렬
-edges.sort()
 
-# 간선 정보 하나씩 확인하면서 크루스칼 알고리즘 수행
-for i in range(e):
-    cost, a, b = edges[i]
-    # find 연산 후, 부모노드 다르면 사이클 발생 X으므로 union 연산 수행 -> 최소 신장 트리에 포함!
-    if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
-        total_cost += cost
-print(parent)
-print(total_cost)
+
+
+
+
+
+
+
+# 예시 코드
+
+# import sys
+
+# v, e = map(int, input().split())
+# # 부모 테이블 초기화
+# parent = [0] * (v+1)
+# for i in range(1, v+1):
+#     parent[i] = i
+
+# # find 연산
+# def find_parent(parent, x):
+#     if parent[x] != x:
+#         parent[x] = find_parent(parent, parent[x])
+#     return parent[x]
+
+# # union 연산
+# def union_parent(parent, a, b):
+#     a = find_parent(parent, a)
+#     b = find_parent(parent, b)
+#     if a < b:
+#         parent[b] = a
+#     else:
+#         parent[a] = b
+
+# # 간선 정보 담을 리스트와 최소 신장 트리 계산 변수 정의
+# edges = []
+# total_cost = 0
+
+# # 간선 정보 주어지고 비용을 기준으로 정렬
+# for _ in range(e):
+#     a, b, cost = map(int, input().split())
+#     edges.append((cost, a, b))
+
+# # 간선 정보 비용 기준으로 오름차순 정렬
+# edges.sort()
+
+# # 간선 정보 하나씩 확인하면서 크루스칼 알고리즘 수행
+# for i in range(e):
+#     cost, a, b = edges[i]
+#     # find 연산 후, 부모노드 다르면 사이클 발생 X으므로 union 연산 수행 -> 최소 신장 트리에 포함!
+#     if find_parent(parent, a) != find_parent(parent, b):
+#         union_parent(parent, a, b)
+#         total_cost += cost
+# print(parent)
+# print(total_cost)
