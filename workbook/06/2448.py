c0cd526a0cc2ce3202acbfa7d63a39053026a86b
choisaywhy <sehwa98@icloud.com>
2022-11-15 02:11:23 +0900

    221115

--- workbook/06/2448.py
+++ workbook/06/2448.py
@@ -1,5 +1,31 @@
 import sys
-sys.setrecrusionlimit(10**6)
+
+input = sys.stdin.readline
 
 def star(N):
-    pass
+    if N == 3:
+        pattern = [[" ", " ", "*", " ", " "],
+                   [" ", "*", " ", "*", " "],
+                   ["*", "*", "*", "*", "*"]]
+        return pattern
+
+    div2 = N//2
+    pattern = star(div2)
+    new_pattern = [[" "]*(2*N - 1) for _ in range(N)]
+
+    for i in range(div2) :
+        new_pattern[i][div2 : div2 + N-1] = pattern[i]
+    for i in range(div2, N) :
+        new_pattern[i] = pattern[i-div2] + [" "] + pattern[i-div2]
+    
+    return new_pattern
+
+N = int(input()) # 3*2^k
+
+pattern = star(N)
+result = ""
+for i in range(len(pattern)) :
+    result += "".join(pattern[i]) + "\n"
+
+
+sys.stdout.write(result.rstrip())
