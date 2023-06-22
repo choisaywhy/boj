dc1c8e072b6cd799ef438bfa5bd35721479256b1
choisaywhy <sehwa98@icloud.com>
2023-06-22 16:02:43 +0900

    230622

--- grd/16987.py
+++ grd/16987.py
@@ -1,41 +1,40 @@
-# https://hangjastar.tistory.com/264 2nd trial 진행 중
+# 2nd trial
+# 무한루프 아니라 걍 개오래 걸리는거였음 ㄷㄷ 통과
 import sys
 
 def solution(n,eggs):
-    ans = []
-    def dfs(idx, bcount):
+    ans = 0
+    def dfs(idx, count):
         nonlocal ans
-        print(idx,bcount,'turn')
+        # print(idx,count,eggs,'turn')
 
-        # if idx == n-1 or bcount == n:
-        #     print(idx,n,'finishing line')
-        #     ans.append(bcount)
-        #     return
-        if idx == n or bcount == n or eggs[idx] <= 0:
-            ans.append(bcount)
+        if idx == n or count == n:
+            if count > ans:
+                ans = count
             return
-        
-        for next in range(n):
-            if next == idx or eggs[next] <= 0 :
-                dfs(idx+1, tcount)
-                continue
-            s1, w1 = eggs[idx]
-            s2, w2 = eggs[next]
-            eggs[idx][0] -= w2
-            eggs[next][0] -= w1
-            tcount = bcount
-            if eggs[idx][0] <= 0:
-                tcount += 1
-            if eggs[next][0] <= 0:
-                tcount += 1
+        if eggs[idx][0] <= 0: # 현재 계란이 이미 깨진 경우
+            dfs(idx+1, count)
+        else:
+            if count == n-1: # 나 빼고 다 깨진 경우
+                dfs(n, count)
+            else:
+                for next in range(n):
+                    if next == idx or eggs[next][0] <= 0 :
+                        continue
+                    eggs[idx][0] -= eggs[next][1]
+                    eggs[next][0] -= eggs[idx][1]
+                    tcount = count
+                    if eggs[idx][0] <= 0:
+                        tcount += 1
+                    if eggs[next][0] <= 0:
+                        tcount += 1
 
-            dfs(idx+1, tcount)
-            eggs[idx][0] = s1
-            eggs[next][0] = s2
+                    dfs(idx+1, tcount)
+                    eggs[idx][0] += eggs[next][1]
+                    eggs[next][0] += eggs[idx][1]
         
     dfs(0,0)
     print(ans)
-    print(max(ans))
 
 if __name__ == "__main__" :
     input = sys.stdin.readline
@@ -44,15 +43,13 @@ if __name__ == "__main__" :
     solution(n,eggs)
 
     
-
-# 무한 루프
+# 1st trial failed
 # import sys
 
 # def solution(n,eggs):
 #     ans = []
 #     def dfs(idx, broken, bcount):
 #         nonlocal ans
-#         print(idx, broken,bcount,'turn')
 
 #         # if idx == n-1 or bcount == n:
 #         #     print(idx,n,'finishing line')
@@ -84,7 +81,6 @@ if __name__ == "__main__" :
 #             broken[next] = False
         
 #     dfs(0, [False]*n,0)
-#     print(ans)
 #     print(max(ans))
 
 # if __name__ == "__main__" :
